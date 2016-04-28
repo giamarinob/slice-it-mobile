d0b22aee938c2c01899d4b2a88c5c7a83d109d17
Zach Barton <email": null>
2016-04-28 08:51:18 -0500

    Fix root url for mobile

--- www/js/app.js
+++ www/js/app.js
@@ -103,10 +103,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
     controller: 'logOutCtrl'
     }
   }
-})
-
-
-  ;
+});
   // if none of the above states are matched, use this as the fallback
-
+  $urlRouterProvider.otherwise('/login');
 });
